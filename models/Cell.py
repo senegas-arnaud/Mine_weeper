01d88cb9f6646c313615a55f8c4e39a4a98a487d
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-03-31 09:55:15 +0200

    Rename Cell.py en cell.py

--- models/Cell.py
+++
@@ -1,7 +0,0 @@
-class Cell:
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-        self.mine = False
-        self.revealed = False
-        self.flag = None
-        self.adjacent_mines = 0
